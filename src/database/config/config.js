d7f886d72536d94164ed7da27de000934971b820
Miguel <miguezaga2@hotmail.com>
2021-12-16 19:49:54 -0500

    first

--- src/database/config/config.js
+++ src/database/config/config.js
@@ -2,7 +2,7 @@ module.exports = {
   "development": {
     "username": "root",
     "password": "1234",
-    "database": "movies_db",
+    "database": "clase33",
     "host": "127.0.0.1",
     "dialect": "mysql",
     "port":"3307"
@@ -10,14 +10,14 @@ module.exports = {
   "test": {
     "username": "root",
     "password": "1234",
-    "database": "movies_db",
+    "database": "clase33",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": "1234",
-    "database": "movies_db",
+    "database": "clase33",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
